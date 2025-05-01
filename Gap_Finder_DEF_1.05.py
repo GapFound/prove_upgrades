fa01753523ad01905ade55fed08cf03adef8e0a8
GapFound <gapfinder@tuta.io>
2025-05-01 18:22:04 +0200

    Update Gap_Finder_DEF_1.05.py

--- Gap_Finder_DEF_1.05.py
+++ Gap_Finder_DEF_1.05.py
@@ -1036,7 +1036,7 @@ with col1:
           
         
     with st.form(key=f'GAPs_Finder'):
-            nome_ticker = st.text_input('**GAPs Finder v1.04pr**',placeholder='Enter the Ticker').strip()
+            nome_ticker = st.text_input('**GAPs Finder v1.05yf**',placeholder='Enter the Ticker').strip()
             bottone_ricerca = st.form_submit_button('ricerca GAPs')
          
     
